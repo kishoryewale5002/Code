91f37363ac900301e4985081d5462389a98f3a1b
kishoryewale5002 <kishoryewale8080@gmail.com>
2025-06-26 13:01:27 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+public class Add {
+ void Add()
+    {
+        int a=10;
+        int b=20;
+        int sum= a + b;
+        System.out.println("The Syum Is :"+sum);
+
+    }
+    public static void main(String[] args) {
+        Add a = new Add();
+        a.Add();
+        
+    }
+}
