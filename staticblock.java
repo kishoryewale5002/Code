02191bd6418ef8fb45ecf0972849d15dce8f0034
kishoryewale5002 <kishoryewale8080@gmail.com>
2025-06-26 13:14:10 +0530

    Add files via upload

---
+++ staticblock.java
@@ -0,0 +1,11 @@
+public class staticblock {
+    static{
+        int a = 1;
+        int b = 4;
+        int add = a+b;
+        System.out.println("addition of a and b is :" +add);
+    }
+    public static void main(String[] args) {
+        System.out.println("this is main method");
+    }
+}
